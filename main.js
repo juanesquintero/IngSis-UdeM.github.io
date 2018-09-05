3b41a3e275d09c8a8772caeb33c10398fd1d306f
juanesquintero <juaneschrome@gmail.com>
2018-09-05 15:05:11 -0500

    Primero

---
+++ main.js
@@ -0,0 +1,18 @@
+/**
+ * Realizar un aplicativo web con HTML, CSS y JAVASCRIPT, el aplicativo debe de contener:
+ *  Página de inicio: Donde se debe describir de que se trata el proyecto y sus objetivos. 
+ *  Página “Sobre Nosotros”: Donde se debe mostrar la foto y la biografía de cada integrante del grupo. 
+ *  Página de login: Una página con un formulario que solicite un email y contraseña para realizar el respectivo inicio de sesión.
+ *  Página de CRUD principal
+ *  Página de CRUD secundario  
+ *  Página de CRUD terciario. 
+    Para la funcionalidad se trabajará con arreglos y objetos.
+
+ * CRUD de Noticias
+ * CRUD de Empresas 
+ * CRUD de Ofertas de Empleo
+ */ 
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
