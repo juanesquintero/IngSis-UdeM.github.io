df282ca32ff62837f03f4509a7d5b752e3eb7f9d
juanesquintero <juaneschrome@gmail.com>
2018-09-07 01:17:18 -0500

    Segundo

--- main.js
+++ main.js
@@ -11,6 +11,17 @@
  * CRUD de Noticias
  * CRUD de Empresas 
  * CRUD de Ofertas de Empleo
+ * 
+ *  <li class="nav-item dropdown">
+        <a href="sobreNosotros.html" class="nav-link dropdown-toggle" data-toggle="dropdown">Sobre Nosotros<b class='caret'></b></a>
+            <ul class="dropdown-menu">
+                <li href="#"><a>Mision</a></li>
+                <li href="#"><a>Vision</a></li>
+                <li href="#"><a>Contacto</a></li>
+            </ul>
+    </li>
+ * 
+ * 
  */ 
 
 function myFunction() {
