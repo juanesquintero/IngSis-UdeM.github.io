a7d21d5cae66ad46f6d9fbf9d18af391ec87093a
juanesquintero <juaneschrome@gmail.com>
2018-09-09 18:32:39 -0500

    Leer Readme

--- js/empresas.js
+++ js/empresas.js
@@ -43,46 +43,69 @@ var app = new function(){
             tel: 3722022,
             email:"danitag@psl.com",            
         }
-
     ]
 
-    getCurso = function(id){
-        asig = ''
-        for(var i=0; i<empresas.length; i++){
-            for(var j=0; j<empresas[i].cursos.length; j++){
-                codigo = empresas[i].cursos[j].id
-                if(codigo == id){
-                    asig = empresas[i].cursos[j].asignatura
-                }
-            }
-        }
-        return asig
-    }
-
     this.edit = function(position){
-        document.getElementById('studentName').value = empresas[position].nombre
-        document.getElementById('studentAge').value = empresas[position].edad
-        document.getElementById('studentCourse').value = empresas[position].cursos[1].asignatura
-        document.getElementById('studentId').value = position
+        document.getElementById('txtNombre').value = empresas[position].nombre
+        document.getElementById('txtActividad').value = empresas[position].actividad
+        document.getElementById('txtAlias').value = empresas[position].alias
+        document.getElementById('pais').value = empresas[position].pais
+        if(empresas[position].pais == "CO"){
+            document.getElementById('divDepar').style.display = "inline"
+            document.getElementById('divEstado').style.display = "none"
+            document.getElementById(empresas[position].estado).selected=true             
+        }else{            
+            document.getElementById('divDepar').style.display = "none"
+            document.getElementById('divEstado').style.display = "inline"
+            document.getElementById('txtEstado').value = empresas[position].estado
+        }
+        document.getElementById('txtCiudad').value = empresas[position].ciudad
+        document.getElementById('txtDic').value = empresas[position].dic
+        document.getElementById('txtWebsite').value = empresas[position].web
+        document.getElementById('txtContacto').value = empresas[position].contacto
+        document.getElementById('txtCargo').value = empresas[position].cargo
+        document.getElementById('txtTelefono').value = empresas[position].tel
+        document.getElementById('txtEmail').value = empresas[position].email
+
         document.getElementById('btnNew').style.display = "none"
         document.getElementById('btnUpdate').style.display = "inline"
         document.getElementById('btnCancel').style.display = "inline"
     }
 
     update = function(){
-        var name = document.getElementById('studentName').value
-        var age = document.getElementById('studentAge').value
-        var curso = document.getElementById('studentCourse').value  
-        var id = document.getElementById('studentId').value
-        var myStudent = { nombre: name, edad:age, cursos:[curso] }
-        empresas.splice(id,1,myStudent)
+        if( checkCampos == true){
+            var name = document.getElementById('txtNombre').value
+            var activity = document.getElementById('txtActividad').value
+            var id = document.getElementById('txtAlias').value
+            var country = document.getElementById('pais').value
+            var state
+            if(country == "CO"){
+                state = document.getElementById('departamento').id
+            }else{
+                state = document.getElementById('txtEstado').value
+            }       
+            var city = document.getElementById('txtCiudad').value
+            var adress = document.getElementById('txtDic').value
+            var page = document.getElementById('txtWebsite').value
+            var contact = document.getElementById('txtContacto').value
+            var charge = document.getElementById('txtCargo').value
+            var phone = document.getElementById('txtTelefono').value
+            var correo = document.getElementById('txtEmail').value
+            var company = { nombre: name, actividad: activity, alias: id, pais: country, estado: state,
+                            ciudad: city, dic: adress, web: page, contacto: contact,cargo: charge ,tel: phone ,email:correo }
+            console.log('Saves: '+company) 
+            empresas.splice({alias:""+id+""},1,company)
+        }else{
+            //mostrar alerta
+            console.log('Fail')
+            document.getElementById('msgAlerta').style.display = "inline"
+        }        
+        limpiar()
         getAll()
     }
 
     cancel = function(){
-        document.getElementById('studentName').value = ''
-        document.getElementById('studentAge').value = ''
-        document.getElementById('studentCourse').value = ''
+        limpiar()
         document.getElementById('btnNew').style.display = "inline"
         document.getElementById('btnUpdate').style.display = "none"
         document.getElementById('btnCancel').style.display = "none"
@@ -95,29 +118,69 @@ var app = new function(){
         getAll()
     }
 
+    checkCampos = function(){
+        if(document.getElementById('txtNombre').value == "" ||
+        document.getElementById('txtActividad').value == "" ||
+        document.getElementById('txtAlias').value == "" ||
+        document.getElementById('pais').value == "" ||
+        document.getElementById('departamento').value == "" ||
+        document.getElementById('txtEstado').value == "" ||
+        document.getElementById('txtCiudad').value == "" ||
+        document.getElementById('txtDic').value == "" ||
+        document.getElementById('txtWebsite').value == "" ||
+        document.getElementById('txtContacto').value == "" ||
+        document.getElementById('txtCargo').value == "" ||
+        document.getElementById('txtTelefono').value == "" ||
+        document.getElementById('txtEmail').value == ""){return false}
+        else{ return true }
+    }
+
+    limpiar = function(){
+        document.getElementById('txtNombre').value = ""
+        document.getElementById('txtActividad').value = ""
+        document.getElementById('txtAlias').value = ""
+        document.getElementById('pais').value = ""
+        document.getElementById('departamento').value = ""
+        document.getElementById('txtEstado').value = ""
+        document.getElementById('txtCiudad').value = ""
+        document.getElementById('txtDic').value = ""
+        document.getElementById('txtWebsite').value = ""
+        document.getElementById('txtContacto').value = ""
+        document.getElementById('txtCargo').value = ""
+        document.getElementById('txtTelefono').value =""
+        document.getElementById('txtEmail').value = ""
+    } 
+
+
     add = function(){
-        var name = document.getElementById('txtNombre').value
-        var activity = document.getElementById('txtActividad').value
-        var alias = document.getElementById('txtAlias').value
-        var country = document.getElementById('pais').value
-        var state
-        if(country == "CO"){
-            state = document.getElementById('departamento').id
+        if(checkCampos()==true){
+            var name = document.getElementById('txtNombre').value
+            var activity = document.getElementById('txtActividad').value
+            var alias = document.getElementById('txtAlias').value
+            var country = document.getElementById('pais').value
+            var state
+            if(country == "CO"){
+                state = document.getElementById('departamento').id
+            }else{
+                state = document.getElementById('txtEstado').value
+            }       
+            var city = document.getElementById('txtCiudad').value
+            var adress = document.getElementById('txtDic').value
+            var page = document.getElementById('txtWebsite').value
+            var contact = document.getElementById('txtContacto').value
+            var charge = document.getElementById('txtCargo').value
+            var phone = document.getElementById('txtTelefono').value
+            var correo = document.getElementById('txtEmail').value
+            var company = { nombre: name, actividad: activity, alias: alias, pais: country, estado: state,
+                            ciudad: city, dic: adress, web: page, contacto: contact,cargo: charge ,tel: phone ,email:correo }
+            console.log('Saves: '+company) 
+            empresas.push(company)
         }else{
-            state = document.getElementById('txtEstado').value
-        }       
-        var city = document.getElementById('txtCiudad').value
-        var adress = document.getElementById('txtDic').value
-        var page = document.getElementById('txtWebsite').value
-        var contact = document.getElementById('txtContacto').value
-        var charge = document.getElementById('txtCargo').value
-        var phone = document.getElementById('txtTelefono').value
-        var correo = document.getElementById('txtEmail').value
-        var company = { nombre: name, actividad: activity, alias: alias, pais: country, estado: state,
-                        ciudad: city, dic: adress, web: page, contacto: contact,cargo: charge ,tel: phone ,email:correo }
-        console.log('Saves: '+company) 
-        empresas.push(company)
-        //limpiar()
+            //mostrar alerta
+            console.log('Fail')
+            document.getElementById('msgAlerta').style.display = "inline"
+        }
+        limpiar()
         getAll()
     }
 
